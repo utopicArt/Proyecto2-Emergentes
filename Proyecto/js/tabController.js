53bde3a275d2c8783d4595fafbad13a9e2543f7b
utopicArt <70866635+utopicArt@users.noreply.github.com>
2023-11-01 01:10:07 -0600

    Weather
    El clima del cliente ahora es visible y funcional

--- Proyecto/js/tabController.js
+++ Proyecto/js/tabController.js
@@ -31,45 +31,51 @@ function searchEngine() {
   }
 }
 
+document.getElementById("details").addEventListener('click', function(){
+  weather();
+});
+
 function weather() {
   //API Key: 29523f5ed8df496a980268afea48f3f3
   //URL: https://api.weatherbit.io/v2.0/current?postal_code=44350&country=MX&units=l&key=29523f5ed8df496a980268afea48f3f3
-  const postalCodeInput = document.querySelector("#postalCodeInput");
+  let out = document.getElementById('out');
+  let postalCodeInput = document.querySelectorAll('#' + 'postalCode')[0]
   let postalCode = postalCodeInput ? postalCodeInput.value : "44630";
 
   const apiKey = "29523f5ed8df496a980268afea48f3f3";
 
-  const apiUrl = `https://api.weatherbit.io/v2.0/current?postal_code=${postalCode}&country=MX&units=l&key=${apiKey}`;
+  let apiUrl = `https://api.weatherbit.io/v2.0/current?postal_code=${postalCode}&country=MX&units=l&key=${apiKey}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
     .then((jsonData) => {
       //show data
-      console.log(data);
-      let weatherData = jsonData.data[0];
-      let city_name = weatherData.city_name;
-      let temp = weatherData.temp;
-      let wind_cdir_full = weatherData.wind_cdir_full;
-      let wind_spd = weatherData.wind_spd;
-      let lat = weatherData.lat;
-      let lon = weatherData.lon;
-      let sunrise = weatherData.sunrise;
-      let sunset = weatherData.sunset;
-      let description = weatherData.weather.description;
-      let icon = weatherData.weather.icon;
+      console.log(jsonData);
+      let weather = jsonData['data'][0];
+      let icon = weather['weather']['icon'];
+      let city_name = weather['city_name'];
+      let temp = weather['temp'];
+      let wind_cdir_full = weather['wind_cdir_full'];
+      let wind_spd = weather['wind_spd'];
+      let lat = weather['lat'];
+      let lon = weather['lon'];
+      let sunrise = weather['sunrise'];
+      let sunset = weather['sunset'];
+      let description = weather['weather']['description'];
+      
 
+      document.getElementById("icon").src = `../images/icons/${icon}.png`;
+      document.getElementById("description").textContent = description;
       document.getElementById("city_name").textContent = city_name;
-      document.getElementById("temp").textContent = temp + "°C";
+      document.getElementById("temp").textContent = '🌡️' + temp + "°C";
       document.getElementById("wind_cdir_full").textContent = wind_cdir_full;
       document.getElementById("wind_spd").textContent = wind_spd;
-      //document.getElementById("lat").textContent = lat;
-      //document.getElementById("lon").textContent = lon;
+      document.getElementById("latLong").textContent = lat + ',  ' + lon;
       document.getElementById("sunrise").textContent = sunrise;
       document.getElementById("sunset").textContent = sunset;
-      document.getElementById("description").textContent = description;
-      document.getElementById("icon").src = `../images/icons/${icon}.png`;
+      
     })
     .catch((error) => {
-      cityName.innerText = "Error al obtener los datos del clima";
+      console.log("Error al obtener los datos del clima: " + error);
     });
 }
